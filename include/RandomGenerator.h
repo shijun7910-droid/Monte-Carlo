8978ff4697397f8b278c787142ddf30969d60d4a
shijun7910-droid <shijun7910@gmail.com>
2025-12-13 13:20:26 +0300

    Update RandomGenerator.h

--- include/RandomGenerator.h
+++ include/RandomGenerator.h
@@ -1,6 +1,7 @@
-#pragma once
+#ifndef RANDOMGENERATOR_H
+#define RANDOMGENERATOR_H
+
 #include <vector>
-#include <random>
 
 class RandomGenerator {
 public:
@@ -9,7 +10,6 @@ public:
     virtual std::vector<std::vector<double>> generateNormalMatrix(int rows, int cols) = 0;
 };
 
-// Mersenne Twister 随机数生成器
 class MersenneTwister : public RandomGenerator {
 private:
     std::mt19937 generator;
@@ -21,18 +21,4 @@ public:
     std::vector<std::vector<double>> generateNormalMatrix(int rows, int cols) override;
 };
 
-// 准随机数生成器 (Sobol序列)
-class SobolGenerator : public RandomGenerator {
-private:
-    unsigned int dimension;
-    unsigned int count;
-    
-public:
-    SobolGenerator(unsigned int dimension = 1);
-    std::vector<double> generateNormal(int n) override;
-    std::vector<std::vector<double>> generateNormalMatrix(int rows, int cols) override;
-    
-private:
-    double sobolSequence(unsigned int index, unsigned int dim);
-    double inverseNormalCDF(double u);
-};
+#endif // RANDOMGENERATOR_H
